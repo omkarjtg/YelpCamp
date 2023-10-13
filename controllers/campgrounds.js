08991b5fc976424d10c5e2d4b48f347358ba4140
omkarjtg <omkarjtg@gmail.com>
2023-10-14 01:46:24 +0530

    Geocoding for new campgrounds

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,9 @@
 const Campground = require("../models/campgrounds");
 const campControllers = {};
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const { cloudinary, storage } = require('../cloudinary');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 
 campControllers.index = async (req, res) => {
@@ -13,10 +16,17 @@ campControllers.renderNewForm = (req, res) => {
 };
 
 campControllers.createNewCamp = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send();
     const newCamp = new Campground(req.body.campground);
+    console.log(req.body);
+    newCamp.geometry = geoData.body.features[0].geometry;
     newCamp.image = req.files.map(file => ({ url: file.path, filename: file.filename }));
     newCamp.author = req.user._id;
     await newCamp.save();
+    console.log(newCamp);
     req.flash('success', 'Added a campground successfully');
     res.redirect(`/campgrounds/${newCamp._id}`);
 };
